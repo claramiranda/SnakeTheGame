4929c3f1b16a02bab0f04ae17fca7f4f3f2b487a
C. Anna Miranda <clara.rosa.miranda@gmail.com>
2019-09-26 15:26:02 -0300

    comidas coloridas
    adicionando parametro de cor aleatória nas comidinhas
    ficou top

--- Food.java
+++ Food.java
@@ -1,22 +1,33 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Food {
 
 	 private int xCoor, yCoor, width, height;
+	 		 Color cor;
 	 
 	 public Food(int xCoor, int yCoor, int tileSize) {
 		 this.xCoor = xCoor;
 		 this.yCoor = yCoor;
 		 width = tileSize;
 		 height = tileSize;
+		 
+		 //gerando cor aleatória para a comidinha
+		 Random randomNumber = new Random();
+		 int color_R, color_G, color_B;
+		 color_R = randomNumber.nextInt(255);
+		 color_G = randomNumber.nextInt(255);
+		 color_B = randomNumber.nextInt(255);
+		 cor = new Color(color_R, color_G, color_B);
 	 }
 	 
 	 public void tick() {
 	  
 	 }
 	 public void draw(Graphics g) {
-		 g.setColor(Color.RED); //colocar cores aleatórias aqui através do rand()
+		 //g.setColor(Color.RED); //colocar cores aleatórias aqui através do rand()
+		 g.setColor(this.cor); 
 		 g.fillRect(xCoor * width, yCoor * height, width, height);
 	 }
 
